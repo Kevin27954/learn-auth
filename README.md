0270916b9789a8a5deb0bb8d1cfaee2dc45bfac5
Kevin27954 <98406775+Kevin27954@users.noreply.github.com>
2024-02-04 21:43:36 -0500

    Added frontend ui login form

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # learn-auth
+This repo is for learning authentication
+The goal is to understand tokens and persistent logins
+
+End result:
+    There should be 3 different methods of authentication:
+    - Personal Implementaion of Auth
+    - Using firebase auth
+    - Using auth0 auth
---
+++ frontend/script.js
@@ -0,0 +1,25 @@
+
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const submitBtnFB = document.getElementById('submit-btn-fb');
+const submitBtnA0 = document.getElementById('submit-btn-a0');
+const submitBtnPA = document.getElementById('submit-btn-pa');
+
+submitBtnFB.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+})
+
+submitBtnA0.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+})
+
+submitBtnPA.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+})
+
+function handleLogin(e) {
+    e.preventDefault();
+}
